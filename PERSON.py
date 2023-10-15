1cba32011813c43c05df76dcdcb30a8ac920581a
Dilmi <dilmithisara343@gmail.com>
2023-10-15 18:34:56 +0530

    Person Class Created

--- PERSON.py
+++ PERSON.py
@@ -1,8 +1,5 @@
 my_list <- list(name = "John", age = 30, city = "New York")
 
-name_value <- my_list["name"]
-age_value <- my_list["age"]
-
-# Print the accessed values
-print(name_value)
-print(age_value)
+# Access list elements
+print(my_list$name)
+print(my_list$age)
